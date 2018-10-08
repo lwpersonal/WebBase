8b17d4a46372edb67c71e1f451d2a45f53154a53
wenjiawei <wenjiawei@innobuddy.com>
2018-10-08 19:03:28 +0800

    完成动态插入

--- demo/demo_1/index.js
+++ demo/demo_1/index.js
@@ -2,22 +2,26 @@
  * @Author: WenJW
  * @Date: 2018-09-27 21:55:58
  * @Last Modified by: WenJW
- * @Last Modified time: 2018-10-08 16:49:36
+ * @Last Modified time: 2018-10-08 18:58:19
  * @description
  */
 
 ;(function(doc, win, undefined){
   var options
-
+  
+  // 工具函数
+  function getDataType(data) {
+    return Object.prototype.toString.call(data).toLowerCase().replace(/^\[object (\w+)\]$/, '$1')
+  }
   function valMap (data, fn, order) {
     order = order || true
-    if (_GetDataType(data) === 'array') {
+    if (getDataType(data) === 'array') {
       order ? null : data.reverse()
       for (var i = 0, len = data.length; i < len; i++) {
         var val = fn(data[i], i)
         if (val === false) { return }
       }
-    } else if (_GetDataType(data) === 'object') {
+    } else if (getDataType(data) === 'object') {
       var keys = Object.keys(data)
       order ? null : keys.reverse()
       for (var i = 0, len = keys.length; i < len; i++) {
@@ -31,7 +35,7 @@
   function Func(config) {
     options = config
     console.log(options.el)
-    options.el.style.whiteSpace = 'pre-line'
+    options.el.style.whiteSpace = 'pre-wrap'
   }
 
   // 创建 html 标签
@@ -61,6 +65,7 @@
 
   // 处理字符串代码
   function handleCode(code) {}
+  // 
   function handvarext(str) {
     var len = str.length
     
@@ -70,17 +75,20 @@
   function writeFn(el, text) {
     if(!text){ return }
     return el.appendData(text)
+    // return el.append(text)
   }
 
   // 注释处理
   function commentHandle(comment, el, fn) {
-    var len = comment.length, index = 0
+    var len = comment.length,
+      index = 0,
+      textEl = createTextNode(el)
     function commentFn(text) {
       // 检查是否有断点
       var time = options.time
       // options.timeOut
       setTimeout(function() {
-        writeFn(el, text)
+        writeFn(textEl, text)
         index++
         if(index < len) { return commentFn(comment[index]) }
         else { return fn() }
@@ -90,13 +98,40 @@
   }
   // 样式处理
   function styleHandle(style, el, fn) {
-    var len = style.length, index = 0
+    var len = style.length,
+      index = 0,
+      root = createNode('span', 'el-select'),
+      textEl = createTextNode(el)
+    el.append(root)
     function styleFn(text) {
-      // 检查是否有断点
-      var time = options.time
       // options.timeOut
+      var time, textIndex = style.indexOf(text)
       setTimeout(function() {
-        writeFn(el, text)
+        // 检查是否是断点，是的话下一个字符延时
+        time = options.time
+        if(text === '{' || text === ';') {
+          textEl = createTextNode(el)
+          writeFn(textEl, text)
+          root = createNode('span', 'el-attr')
+          textEl = createTextNode(root)
+        } else if(text === ':') {
+          textEl = createTextNode(el)
+          writeFn(textEl, text)
+          root = createNode('span', 'el-val')
+          textEl = createTextNode(root)
+        } else if(text === '}') {
+          writeFn(textEl, text)
+          root = createNode('span', 'el-select')
+          textEl = createTextNode(root)
+        } else if(/\s/g.test()) {
+          console.log('ssssdsf')
+          textEl = createTextNode(el)
+          writeFn(textEl, text)
+        } else {
+          console.log('sss')
+          el.append(root)
+          writeFn(textEl, text)
+        }
         index++
         if(index < len) { return styleFn(style[index]) }
         else { return fn() }
@@ -110,9 +145,9 @@
     var el
     if(/\/\*/g.test(str)) {
       el = createNode('div', 'comment')
-      var textEl = createTextNode(el)
+      // var textEl = createTextNode(el)
       options.el.append(el)
-      commentHandle(str, textEl, fn)
+      commentHandle(str, el, fn)
     } else if(/\{[\w\W]+\}/g.test(str)) {
       el = createNode('div', 'code')
       options.el.append(el)
