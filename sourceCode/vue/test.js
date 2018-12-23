b757acac30622f82e44819c5e75ac257278c51ae
WenJW <17648253663@163.com>
2018-12-23 22:26:20 +0800

    add defineProperty test code

--- sourceCode/vue/test.js
+++ sourceCode/vue/test.js
@@ -34,4 +34,51 @@
   // console.log(obj.name)
   // obj.name = 'lg'
   // console.log(obj.name)
+
+
+  // proxy
+  const obj2 = new Proxy({}, {
+    get: function (target, key, receiver) {
+      console.log(`getting ${key}!`)
+      return Reflect.get(target, key, receiver)
+    },
+    set: function (target, key, value, receiver) {
+      console.log(`setting ${key}!`)
+      return Reflect.set(target, key, value, receiver)
+    }
+  })
+
+  obj2.count = 1
+  ++obj2.count
+
+  const proxy1 = new Proxy({}, {
+    get: function() {
+      return 22
+    }
+  })
+  console.log(proxy1)
+  proxy1.count = 222
+  console.log(proxy1.count)
+  console.log(proxy1.num)
+
+  // Reflect
+  const obj3 = {
+    name: 'lw',
+    age: 24
+  }
+  const loggedObj = new Proxy(obj3, {
+    get(target, name) {
+      console.log('get: ', 'target - ', name)
+      return Reflect.get(target, name)
+    },
+    has(target, name) {
+      console.log('has: ', 'target - ', name)
+      return Reflect.has(target, name)
+    }
+  })
+  console.log(loggedObj.name)
+  console.log(loggedObj.age)
+  'age' in loggedObj
+
+
 }())
