b4fad24875cb75d680294650c9892b903906250d
wenjw <17648253663@163.com>
2020-07-05 11:52:53 +0800

    珠峰课程，函数封装

--- zhufeng/utils.js
+++ zhufeng/utils.js
@@ -8,8 +8,6 @@ function isType (type) {
     return Object.prototype.toString.call(val) === `[object ${type}]`;
   };
 };
-
-
 // console.log(isType('String')('test'));
 
 /**
@@ -27,14 +25,60 @@ function curring(fn) {
   }
   return handleCurr;
 };
-
 /*
-
 function add(a, b, c, e) {
   return a + b + c + e;
 };
 const add2 = curring(add);
 console.log(add2(1)(3)(6)(5)()); // 15
 console.log(add2(1)(3, 6)(5)()); // 15
+*/
 
+/**
+ * @description 延迟函数执行
+ * @param {*} callBack
+ * @param {*} delay
+ * @returns
+ */
+function after(callBack, delay) {
+  return function fn() {
+    if (--delay===0) { return callBack() };
+    return fn;
+  };
+};
+/*
+function eat() {
+  console.log('吃饭');
+};
+
+const fn = after(eat, 3);
+fn();
+fn();
+fn();
+*/
+
+/**
+ * @description 前置函数
+ * @param {*} callBack
+ * @returns
+ *
+ * AOP 函数切片思想
+ * */
+Function.prototype.before = function(callBack) {
+  const that = this;
+  return function(...args) {
+    const res = callBack();
+    return that(...args, res);
+  }
+}
+/*
+function say(name, name2) {
+  console.log(name + ': 说话 ' + name2);
+}
+const fn = say.before(function() {
+  console.log('小明站了起来，');
+  return '小红';
+});
+
+fn('然后');
 */
