d466368628b88d7467666fd02fdf922b5c8343d4
wenjiawei01 <wenjiawei01@58.com>
2021-06-28 19:47:20 +0800

    feat: 链表反转，回文串

--- algorithm/index.js
+++ algorithm/index.js
@@ -1,4 +1,4 @@
-;(function (doc, win, undefined) {
+(function (doc, win, undefined) {
   // *********
   // 费式数列
 
@@ -6,18 +6,18 @@
   // 小兔子一个月后生小兔子
   // 1 / 1 = 2 / 2 = 3
   const fn_1 = (num_1, num_2, n) => {
-    let rabbat = num_1 || 0
-    let youngRabbat = num_2 || 0
-    let all = rabbat + youngRabbat
+    let rabbat = num_1 || 0;
+    let youngRabbat = num_2 || 0;
+    let all = rabbat + youngRabbat;
     if (n === 0) {
       return console.log(
         'res: ',
         `${rabbat}只大兔子，${youngRabbat}只小兔子，共${all}只兔子！`
-      )
+      );
     } else {
-      return fn_1(rabbat + youngRabbat, rabbat, n - 1)
+      return fn_1(rabbat + youngRabbat, rabbat, n - 1);
     }
-  }
+  };
   // fn_1(2, 1, 1)
   // fn_1(1, 0, 3)
 
@@ -36,97 +36,97 @@
   //  1(0)  1(1)
   // 1(0)  2(1)  1(2)
   //1(0)  3(1)  3(2)  1(3)
-  const fn_2 = (n) => {
-    let resArr = []
-    const f_1 = (num) => {
+  const fn_2 = n => {
+    let resArr = [];
+    const f_1 = num => {
       if (num > n) {
-        return resArr
+        return resArr;
       } else {
-        let arr = []
-        const beginArr = resArr[num - 2]
+        let arr = [];
+        const beginArr = resArr[num - 2];
 
-        console.log('num', beginArr)
+        console.log('num', beginArr);
         for (let i = 0; i < num; i++) {
           arr[i] =
             beginArr !== void 0 &&
             beginArr[i - 1] !== void 0 &&
             beginArr[i] !== void 0
               ? beginArr[i - 1] + beginArr[i]
-              : 1
+              : 1;
         }
-        console.log(arr)
-        resArr[num - 1] = arr
-        return f_1(num + 1)
+        console.log(arr);
+        resArr[num - 1] = arr;
+        return f_1(num + 1);
       }
-    }
-    return f_1(1)
-  }
+    };
+    return f_1(1);
+  };
   // console.log(fn_2(9))
 
   // 排序
   // 直接插入排序
-  const px_fn = (arr) => {
+  const px_fn = arr => {
     let temp,
       i,
       j,
-      len = arr.length
+      len = arr.length;
     for (i = 1; i < len; i++) {
       if (arr[i] < arr[i - 1]) {
-        temp = arr[i]
-        arr[i] = arr[i - 1]
+        temp = arr[i];
+        arr[i] = arr[i - 1];
         for (j = i - 2; arr[j] > temp && j >= 0; j--) {
-          arr[j + 1] = arr[j]
+          arr[j + 1] = arr[j];
         }
-        arr[j + 1] = temp
+        arr[j + 1] = temp;
       }
     }
-    return arr
-  }
-  console.log(px_fn([4, 47, 6, 7, 2, 4, 1, 3, 4]))
+    return arr;
+  };
+  // console.log(px_fn([4, 47, 6, 7, 2, 4, 1, 3, 4]))
 
   // 冒泡排序
-  const px_fn_mp = (arr) => {
+  const px_fn_mp = arr => {
     let temp,
       i,
       j,
       len = arr.length,
-      tag = 1
+      tag = 1;
     for (i = 0; i < len && tag; i++) {
-      tag = 0
+      tag = 0;
       for (j = 0; j < len - i; j++) {
         if (arr[j] > arr[j + 1]) {
-          arr[j + 1] = [arr[j], (arr[j] = arr[j + 1])][0]
-          tag = 1
+          arr[j + 1] = [arr[j], (arr[j] = arr[j + 1])][0];
+          tag = 1;
         }
       }
     }
-    return arr
-  }
-  console.log(px_fn_mp([47, 4, 6, 7, 2, 4, 1, 3, 4]))
-  console.log(px_fn_mp([1, 4, 6, 7, 2, 4, 1, 3, 4]))
-  console.log(px_fn_mp([6, 4, 1]))
+    return arr;
+  };
+  // console.log(px_fn_mp([47, 4, 6, 7, 2, 4, 1, 3, 4]))
+  // console.log(px_fn_mp([1, 4, 6, 7, 2, 4, 1, 3, 4]))
+  // console.log(px_fn_mp([6, 4, 1]))
 
   // 简单选择排序
-  const px_fn_jdxz = (arr) => {
+  const px_fn_jdxz = arr => {
     let temp,
       i,
       j,
       len = arr.length,
-      k
+      k;
     for (i = 0; i < len - 1; i++) {
-      k = i
+      k = i;
       for (j = i + 1; j < len; j++) {
         if (arr[k] > arr[j]) {
-          k = j
+          k = j;
         }
       }
-      temp = arr[i]
-      arr[i] = arr[k]
-      arr[k] = temp
+      temp = arr[i];
+      arr[i] = arr[k];
+      arr[k] = temp;
     }
-    return arr
-  }
-  console.log(px_fn_jdxz([47, 4, 6, 7, 2, 4, 1, 3, 4]))
+    return arr;
+  };
+  // console.log(px_fn_jdxz([47, 4, 6, 7, 2, 4, 1, 3, 4]))
 
   // 希尔排序
   // const px_fn_xe = arr => {
@@ -135,44 +135,122 @@
   // }
 
   // 快排
-  const px_fn_k = (arr) => {
+  const px_fn_k = arr => {
     const fn = (arr1, arr2) => {
-      console.log('===')
-      console.log(arr1, arr2)
-      let num = arr1[0]
+      let num = arr1[0];
       if (arr1.length > 1) {
-        arr1 = arr1.filter((item) => {
+        arr1 = arr1.filter(item => {
           if (item > num) {
-            arr2.push(item)
+            arr2.push(item);
           }
-          return item <= num
-        })
+          return item <= num;
+        });
         fn(
           arr1.slice(0, Math.floor(arr1.length / 2)),
           arr1.slice(Math.floor(arr1.length / 2))
-        )
+        );
       }
       if (arr2.length > 1) {
-        arr2 = arr2.filter((item) => {
+        arr2 = arr2.filter(item => {
           if (item > num) {
-            arr1.push(item)
+            arr1.push(item);
           }
-          return item <= num
-        })
+          return item <= num;
+        });
         fn(
           arr2.slice(0, Math.floor(arr2.length / 2)),
           arr2.slice(Math.floor(arr2.length / 2))
-        )
+        );
       }
-      return [...arr1, ...arr2]
-    }
+      return [...arr1, ...arr2];
+    };
     return fn(
       arr.slice(0, Math.floor(arr.length / 2)),
       arr.slice(Math.floor(arr.length / 2))
-    )
+    );
+  };
+  // console.log(px_fn_k([4, 5, 7, 84, 1, 4, 5, 6]))
+
+
+  // TAG 链表
+  class List {
+    constructor(value, next) {
+      this.value = value;
+      this.next = next;
+    }
   }
-  console.log(px_fn_k([4, 5, 7, 84, 1, 4, 5, 6]))
-})(document, window)
+
+  function pushStr(str) {
+    if (!str) {
+      return null;
+    }
+    let head = new List(null, null);
+    const n = str.length;
+    let current = head;
+    for (let i = 0; i < n; i++) {
+      current.value = str[i];
+      current.next = i < n - 1 ? new List(null, null) : null;
+      current = current.next;
+    }
+    return head;
+  }
+  const headList = pushStr('abc', null);
+
+  // 回文字符串检测
+  function checkStr(head) {
+    if (head === null) {
+      return false;
+    }
+    let current1 = head;
+    let current2 = head;
+    let str1 = '';
+    let str2 = '';
+    while (current2) {
+      str1 += current1.value;
+      current2 = current2?.next?.next;
+      if (current2) {
+        current1 = current1.next;
+      }
+    }
+    while (current1.next) {
+      str2 = current1.next.value + str2;
+      current1 = current1.next;
+    }
+    if (str1.length === str2.length) {
+      return str1 === str2;
+    } else if (str1.length > str2.length) {
+      return str1.slice(0, -1) === str2;
+    }
+  }
+
+  // console.log(checkStr(headList));
+
+  // 反转单链表
+  function reverseList(head) {
+    let current = head;
+    let resList = null;
+    while (current) {
+      const newList = new List(current.value, resList);
+      resList = newList;
+      current = current.next;
+    }
+    return resList;
+  }
+  // console.log(reverseList(headList));
+
+  function reverseList2(head) {
+    let current = head;
+    let resList = null;
+    while (current) {
+      const next = current.next;
+      current.next = resList;
+      resList = current;
+      current = next;
+    }
+    return resList;
+  }
+  // console.log(reverseList2(headList));
+})(document, window);
 
 // 1
 // 1 1 1
